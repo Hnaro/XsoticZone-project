f2e3f7ecc9758e45fd38faf5d34c840141aff43b
Hnaro Serrano <arhonprincipe16@gmail.com>
2023-08-17 19:54:12 -0600

    updated

--- router/xsoticZoneRoutes.mjs
+++ router/xsoticZoneRoutes.mjs
@@ -6,22 +6,35 @@ import { json } from 'node:stream/consumers';
 // watch movement of each player record it 
 router.post('/matchMove', async (req, res) => {
     // database collections
+    // matches collection
     const matchCollections = db.collection("matches");
-    const moveModel = {
-        move: req.body.move,
-        player: req.body.playerName
-    }
-    console.log(moveModel)
-    console.log(req.body);
-    const results = await matchCollections.insertOne(moveModel);
-    console.log(results);
-    //const results = await matchCollections.updateOne({player: "john"},{move:"c2r1"})
-    if (req.body) {
-        res.send({ response: "status ok", 
-        sentName: req.body.playerName, 
-        sentMove: req.body.move })
-    } else {
-        res.send({response: "status error"});
+    try {
+        // first check session before insrting the data 
+        const moveModel = {
+            sesssionUUID: req.body.sessUUID,
+            playerUUID: req.body.playerUUID,
+            playerMove: req.body.playerMove
+        }
+        // if session is already in the database update the playerMove 
+        const dbsessionUUID = await matchCollections
+        .findOne({sesssionUUID: req.body.sessUUID}, moveModel);
+        // and playerUUID if not existing then insertOne
+        if (!dbsessionUUID) {
+            const results = await matchCollections.insertOne(moveModel);
+        }
+        console.log(results);
+        //const results = await matchCollections.updateOne({player: "john"},{move:"c2r1"})
+        if (req.body) {
+            res.send({ response: "status ok", 
+            sentName: req.body.playerName, 
+            sentMove: req.body.move })
+        } else {
+            res.send({response: "status error"});
+        }
+    } catch(err) {
+        console.log(err)
+    } finally {
+    
     }
 });
 
