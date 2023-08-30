2c9b01e95822af002bfc233c860e0bf4a913078e
Hnaro Serrano <arhonprincipe16@gmail.com>
2023-08-30 16:59:01 -0600

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'
-import { TictactoeGameComponent } from './mini-games/tictactoe-game/tictactoe-game.component';
 import { BackendServiceService } from './services/backend-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TictactoeGameComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
