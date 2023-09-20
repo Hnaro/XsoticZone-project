e3acf6dd8718caada2023890d55a97c9f044b150
Hnaro <arhonprincipe16@gmail.com>
2023-09-20 17:29:52 -0600

    updated

--- router/xsoticZoneRoutes.mjs
+++ router/xsoticZoneRoutes.mjs
@@ -1,15 +1,20 @@
 import * as express from 'express';
 export const router = express.Router();
 import { db } from '../app.mjs';
-import { MoveModel } from '../model/moveModel.js';
+import { v4 as uuidv4 } from 'uuid';
 
-// watch movement of each player record it 
+// generate uuid 
+router.get('/generateUUID', async (req, res) => {
+    // setup UUID here for player or match uuid
+    res.send(uuidv4());
+});
+
+// watch movement of each player record eit 
 router.post('/matchMove', async (req, res) => {
     // database collections
     // matches collection
     const matchCollections = db.collection("matches");
-<<<<<<< HEAD
-    const moveModel = {
+    const moveModel = { 
         move: req.body.move,
         player: req.body.playerName
     }
@@ -25,35 +30,6 @@ router.post('/matchMove', async (req, res) => {
     })
     } else {
         res.send({response: "status error"});
-=======
-    try {
-        // first check session before insrting the data 
-        const moveModel = {
-            sesssionUUID: req.body.sessUUID,
-            playerUUID: req.body.playerUUID,
-            playerMove: req.body.playerMove
-        }
-        // if session is already in the database update the playerMove 
-        const dbsessionUUID = await matchCollections
-        .findOne({sesssionUUID: req.body.sessUUID}, moveModel);
-        // and playerUUID if not existing then insertOne
-        if (!dbsessionUUID) {
-            const results = await matchCollections.insertOne(moveModel);
-        }
-        console.log(results);
-        //const results = await matchCollections.updateOne({player: "john"},{move:"c2r1"})
-        if (req.body) {
-            res.send({ response: "status ok", 
-            sentName: req.body.playerName, 
-            sentMove: req.body.move })
-        } else {
-            res.send({response: "status error"});
-        }
-    } catch(err) {
-        console.log(err)
-    } finally {
-    
->>>>>>> 1dacf2564af03ab0a634f43dc93467630e052971
     }
 });
 
